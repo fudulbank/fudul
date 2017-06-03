94ae8eaad04901fb4eda8b705d13928e8482fb96
User 1 <user1@fudulbank.com>
2017-06-03 16:09:08 +0300

    Expand settings

---
+++ fudul/secrets.template.py
@@ -0,0 +1,29 @@
+# These are the instance-dependent settings.  Copy this file to
+# secrets.py and apply the desired settings.
+# 
+# Only one variable is required here, SECRET_KEY.  Fill this using:
+# http://www.miniwebtool.com/django-secret-key-generator/
+SECRET_KEY = ''
+
+# You may optionally want to copy the following keys from the
+# production server, if you are going to deal with them:
+
+#SOCIAL_AUTH_TWITTER_KEY =
+#SOCIAL_AUTH_TWITTER_SECRET =
+
+# In your development setup, you can leave the following variables
+# unset:
+
+#STATIC_ROOT =
+#MEDIA_ROOT =
+#DEBUG =
+#DATABASES =
+#EMAIL_BACKEND =
+#EMAIL_USE_TLS =
+#EMAIL_HOST =
+#EMAIL_PORT =
+#EMAIL_HOST_USER =
+#EMAIL_HOST_PASSWORD =
+#SESSION_COOKIE_DOMAIN =
+#CSRF_COOKIE_DOMAIN =
+#SECURE_HSTS_SECONDS =
