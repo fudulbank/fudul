210786a7119bf57398f4129ce2b598965d5be90c
User 1 <user1@fudulbank.com>
2017-06-13 23:52:41 +0300

    Add teams to the admin, with autocomplete

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^ajax/get_users$', views.UserAutocomplete.as_view(), name='user_autocomplete'),
     url(r'^$', views.show_index, name="index"),
 ]
