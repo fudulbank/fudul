193c22cf06531fdb2e58d1a51349fdf10b80be5b
User 1 <user1@fudulbank.com>
2017-08-24 20:34:05 +0300

    Various cleanups and fixes

--- accounts/templatetags/account_utils.py
+++ accounts/templatetags/account_utils.py
@@ -1,7 +1,14 @@
 from django import template
+from accounts import utils
+
 
 register = template.Library()
 
 @register.filter
 def is_user_allowed_to_institution(user, institution):
     return institution.is_user_allowed(user)
+
+@register.simple_tag
+def get_user_representation(user, with_email=False, with_nickname=True):
+    return utils.get_user_representation(user, with_email=with_email,
+                                         with_nickname=with_nickname)
