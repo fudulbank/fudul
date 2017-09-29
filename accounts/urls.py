7b9b666acf6c8992cda86839dd127e4e51c39223
User 2 <user2@fudulbank.com>
2017-09-29 15:50:09 +0300

    fix_userena_profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from . import views
-
+from . import views , forms
+from userena import views as userenaviews
 
 urlpatterns = [
     url(r'^details$', views.get_institution_details, name='get_institution_details'),
     url(r'^signup/$', views.signup, name="signup"),
+    url(r'^(?P<username>[\.\w-]+)/edit/$', userenaviews.profile_edit,{'edit_profile_form': forms.CustomEditProfileForm}, name='userena_profile_edit'),
     url(r'', include('userena.urls')),
 ]
