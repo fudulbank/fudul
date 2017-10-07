c34ad96f2a683711d2b3a45c03ef4754bea2742d
User 2 <user2@fudulbank.com>
2017-10-07 23:51:45 +0300

    clean_up

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     url(r'^ajax/get_users$', views.UserAutocomplete.as_view(), name='user_autocomplete'),
     url(r'^about/$', views.show_about, name="about"),
     url(r'^$', views.show_index, name="index"),
-    url(r'^latest/(?:(?P<user_pk>\d+)/)?$', views.list_latest_changes, name="list_latest_changes"),
 ]
