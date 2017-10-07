0a5f43cad268f4a36ecec8eeaf12162fa825c6f3
User 2 <user2@fudulbank.com>
2017-10-07 23:46:57 +0300

    keep_redirect

--- core/views.py
+++ core/views.py
@@ -28,8 +28,9 @@ def show_index(request):
             context['added_question_count'] = added_question_count
             edited_question_count = revision_pool.exclude(is_first=True).count()
             context['edited_question_count'] = edited_question_count
-        return render(request, 'index.html', context)
-
+            return render(request, 'index.html', context)
+        else:
+            return render(request, 'count-down.html', context)
     else:
         question_count = exams_models.Question.objects.undeleted().count()
         answer_count = exams_models.Answer.objects.count()
@@ -71,7 +72,3 @@ def show_about(request):
                'editor_count': editor_count}
 
     return render(request, 'about.html', context)
-
-def list_latest_changes(request, user_pk=None):
-    pass
-    return render(request, 'list_latest_changes.html')
