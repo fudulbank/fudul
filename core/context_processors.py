e38df133fdb13adaf998b1c25df478752753ae52
User 1 <user1@fudulbank.com>
2017-12-10 22:04:02 +0300

    remove unneeded context variables

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,13 +6,8 @@ from exams.models import Session, Revision, Category
 def universal_context(request):
     if request.user.is_authenticated():
         is_any_editor = teams.utils.is_editor(request.user)
-        privileged_exams = exams.utils.get_user_privileged_exams(request.user)
-        pending_revision_count = Revision.objects.undeleted()\
-                                                 .filter(question__exam__in=privileged_exams)\
-                                                 .count()
         meta_categories = Category.objects.meta()
         return {'is_any_editor': is_any_editor,
-                'meta_categories': meta_categories,
-                'pending_revision_count': pending_revision_count}
+                'meta_categories': meta_categories}
     else:
         return {}
