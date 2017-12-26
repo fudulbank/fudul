1b432b5cce621f33aead3844de4acc85da245b1e
User 1 <user1@fudulbank.com>
2017-12-26 19:27:06 +0300

    Add retention curve

---
+++ scripts/update_retention_stats.sh
@@ -0,0 +1,21 @@
+ROOT_DIR=`dirname $(dirname $0)`
+INDICATOR_FILE=$ROOT_DIR/privileged_files/indicators/retention.csv
+
+psql -d fudul_django fudul_django -c  "COPY(
+    SELECT active_days, cumulative_sum FROM (
+        SELECT active_days, cumulative_sum, MAX(cumulative_sum) OVER () as max_sum FROM (
+            SELECT active_days, SUM(total_count) OVER (ORDER BY active_days DESC) as cumulative_sum FROM (
+                SELECT active_days, count(*) as total_count FROM (
+                    SELECT count(distinct date_trunc('day', submission_date)) as active_days FROM (
+                        SELECT auth_user.id, exams_answer.submission_date FROM exams_answer
+                        INNER JOIN exams_session ON (exams_answer.session_id = exams_session.id)
+                        INNER JOIN auth_user ON (exams_session.submitter_id=auth_user.id)
+                        UNION
+                        SELECT auth_user.id, exams_session.submission_date FROM exams_session
+                        INNER JOIN auth_user ON (exams_session.submitter_id=auth_user.id)
+                    ) as unique_dates GROUP BY id
+                ) as totals GROUP BY active_days
+            ) as uncumulative
+        ) as cumulative 
+    ) as top_totals WHERE cumulative_sum >= (max_sum * 0.05) ORDER BY active_days
+) To STDOUT With CSV HEADER;" > $INDICATOR_FILE
