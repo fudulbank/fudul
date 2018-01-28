f5fbac13ba5db128f44e57849ba20e41949dd6d9
User 1 <user1@fudulbank.com>
2018-01-29 00:32:56 +0300

    Add rel="noopener" to external links

--- core/templatetags/core_utils.py
+++ core/templatetags/core_utils.py
@@ -8,4 +8,4 @@ register = template.Library()
 @register.filter
 @stringfilter
 def urlize_target_blank(value, autoescape=None):
-    return mark_safe(urlize_impl(value, nofollow=True, autoescape=autoescape).replace('<a', '<a target="_blank"'))
+    return mark_safe(urlize_impl(value, nofollow=True, autoescape=autoescape).replace('<a', '<a rel="noopener" target="_blank"'))
