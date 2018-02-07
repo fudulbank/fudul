d05de67b098236a71d9685e7f2f8c1137af5bbe4
User 1 <user1@fudulbank.com>
2018-02-07 11:08:45 +0300

    Add Highlight api

--- exams/api.py
+++ exams/api.py
@@ -1,14 +1,17 @@
-from exams.models import Answer, Session
+from exams.models import Answer, Session, Highlight
 from rest_framework import serializers, viewsets, permissions
 from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 
-
-# Serializers define the API representation.
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ('question_id', 'choice_id')
+
+class HighlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Highlight
+        fields = ('revision_id', 'highlighted_text', 'stricken_choices')
 
 class HasSessionAccess(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -22,7 +25,6 @@ class HasSessionAccess(permissions.BasePermission):
             session = get_object_or_404(Session, pk=session_pk)
             return session.submitter == request.user
 
-# ViewSets define the view behavior.
 class AnswerViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AnswerSerializer
     permission_classes = (HasSessionAccess,)
@@ -31,3 +33,12 @@ class AnswerViewSet(viewsets.ReadOnlyModelViewSet):
         session_pk = self.request.query_params.get('session_pk')
         return Answer.objects.filter(session_id=session_pk,
                                      session__is_deleted=False)
+
+class HighlightViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = HighlightSerializer
+    permission_classes = (HasSessionAccess,)
+
+    def get_queryset(self):
+        session_pk = self.request.query_params.get('session_pk')
+        return Highlight.objects.filter(session_id=session_pk,
+                                        session__is_deleted=False)
