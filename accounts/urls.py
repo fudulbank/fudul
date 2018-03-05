6a25c6177a2eef5f2201073222967d3df88bdfb2
User 1 <user1@fudulbank.com>
2018-03-05 21:36:10 +0300

    Override userena's activate

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import url, include
+from django.core.urlresolvers import reverse_lazy
 from . import views , forms
-from userena import views as userenaviews
+from userena import views as userena_views
 
 urlpatterns = [
     url(r'^details$', views.get_institution_details, name='get_institution_details'),
     url(r'^signup/$', views.signup, name="signup"),
+
+    # Override userena's activate view to change the success_url
+    url(r'^activate/(?P<activation_key>\w+)/$',
+       userena_views.activate,
+       {'success_url': reverse_lazy('index')},
+       name='userena_activate'),
+
     url(r'^(?P<username>[\.\w-]+)/edit/$', views.edit_profile, name='userena_profile_edit'),
     # Change personal_email and confirm it
     url(r'^(?P<username>[\@\.\w-]+)/personal_email/$',
