36aae9828f33f2f6c49e8c58820ce0cd7aa7e212
User 1 <user1@fudulbank.com>
2018-06-16 13:12:14 +0300

    Handle small-case answer column, improve TAIF rule

--- exams/management/commands/convert_to_csv.py
+++ exams/management/commands/convert_to_csv.py
@@ -4,11 +4,17 @@ import csv
 import sys
 import re
 
-regex_patterns = {'PEDIATRIC_PART_I': {'full_question': ' +\d+[-\)]\s+(?P<question_text>.+\n+(?:.+\n+)*?)(?P<choices>(?: *\(?[a-f][\-\)\.] .+\n)+)\s*(?:The answer is|Answer|Answer is|The answer|The Correct answer is)\s+\(?(?P<answer>[A-F])\)?\s+\(?\s*(?P<subject>[^\)]+?)\s*\)?\s+(?:Explanation\s*:\s+(?P<explanation>.+\n+(?:.+\n+)*?)?(?= +|END))',
+regex_patterns = {'PEDIATRIC_PART_I': {'full_question': ' +\d+[\.\-\)]\s+(?P<question_text>.+\n+(?:.+\n+)*?)(?P<choices>(?: *\(?[a-f][\-\)\.] .+\n)+)\s*(?:The answer is|Answer|Answer is|The answer|The Correct answer is)\s+\(?(?P<answer>[A-F])\)?\s+\(?\s*(?P<subject>[^\)]+?)\s*\)?\s+(?:Explanation\s*:?\s+(?P<explanation>.+\n+(?:.+\n+)*?)?(?= +|END))',
                                 'choice': ' *\(?[a-f][\)\.] (.+)\n',
                                 'explanation': ''},
                   'PEDIATRIC_PROMOTION': {'full_question': '^(?P<question_sequence>\d+)\.\s*(?P<question_text>.+(\n.+)*?)\n(?P<choices>(?: *\(?[a-f][\-\)\.] .+\n)+)\s*',
-                                          'choice': ' *\(?[a-f][\)\.] (.+)\n'}
+                                          'choice': ' *\(?[a-f][\)\-\.] (.+)\n'},
+                  'PEDIATRIC_ARAB_BOARD': {'full_question': '^(?P<question_sequence>\d+)\.\s+(?P<question_text>.+\n+(?:.+\n+)*?)(?P<choices>(?: *\(?[a-f][\-\)\.] .+\n)+)',
+                                'choice': ' *\(?[a-f][\)\-\.] (.+)\n'},
+                  'TAIF': {'full_question': '^(?P<question_sequence>\d+)\.\s*(?P<question_text>.+\n+(?:.+\n+)*?)(?P<choices>(?:^ *\(?[a-f][\-\)\.,] *.+\n)+)\s*The answer is\s+(?P<answer>[A-F])',
+                                'choice': '^ *\(?[a-f][\)\-\.] *(.+)\n'},
+                  'PNU': {'full_question': '^ *(?P<question_sequence>\d+)\.\s*(?P<question_text>.+\n+(?:.+\n+)*?)(?P<choices>(?: *\(?[a-f][\-\)\.] *.+\n)+)\s*?(?:Answer is\s+(?P<answer>[A-F]))?\s+?',
+                                'choice': ' *\(?[a-f][\)\-\.] *(.+)\n'},
 }
 
 class Command(BaseCommand):
@@ -32,6 +38,7 @@ class Command(BaseCommand):
 
         if options['answer_csv_file']:
             answers = {}
+            explanations = {}
             with open(options['answer_csv_file']) as f:
                 csv_reader = csv.reader(f)
                 for row in csv_reader:
@@ -40,7 +47,13 @@ class Command(BaseCommand):
                         continue
                     sequence = row[0]
                     answer = row[1]
-                    answers[sequence] = answer
+                    answers[sequence] = answer.upper()
+                    try:
+                        explanation = row[2]
+                    except IndexError:
+                        pass
+                    else:
+                        explanations[sequence] = explanation
 
         rule = options['rule']
         source = options['source']
@@ -67,11 +80,16 @@ class Command(BaseCommand):
                reference = ''
             if options['answer_csv_file']:
                 answer = answers.get(sequence, '')
+                explanation = explanations.get(sequence, '')
             else:
                 try:
                     answer = matched_question.group('answer')
                 except IndexError:
                     answer = ''
+                try:
+                    explanation = matched_question.group('explanation').strip()
+                except IndexError:
+                    explanation = ''
             subjects = ['', '', '']
             try:
                 subjects = [matched_question.group('subject')] + subjects
@@ -84,10 +102,6 @@ class Command(BaseCommand):
                 question_text = question_text.replace('\n', ' ')
                 question_text = question_text.replace('  ', ' ')
             choices_str = matched_question.group('choices')
-            try:
-                explanation = matched_question.group('explanation').strip()
-            except IndexError:
-                explanation = ''
             choice_list = []
             for choice in re.finditer(regex_patterns[rule]['choice'], choices_str, re.M | re.I):
                 choice_text = choice.group(1)
